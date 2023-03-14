4072d591b61b9ae4654bed68b6ac4eb47f06c2e9
Manish <manish@dowellresearch.in>
2023-03-15 00:50:42 +0530

    Added sever status

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from serverstatus import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', serverReports.as_view()),
     path('api/',(include('mailapp.urls')))
 ]
