e919412321d04b091b895ffc8284754a55c80992
Manish <manish@dowellresearch.in>
2023-05-30 03:38:50 +0530

    is_valid field to 3

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class ApiKey(models.Model):
     uuid = models.CharField(max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
-    is_valid = models.IntegerField(default=14)
+    is_valid = models.IntegerField(default=3)
 
     def __str__(self):
         return str(self.uuid)
