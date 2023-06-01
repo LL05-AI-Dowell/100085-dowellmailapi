d38797b27b8a95f77b064374430541249874d486
Manish <mdashsharma95@gmail.com>
2023-06-01 06:15:29 +0530

    Corrected server log

--- serverstatus.py
+++ serverstatus.py
@@ -8,5 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 @method_decorator(csrf_exempt, name='dispatch')
 class serverReports(APIView):
     def get(self, request ):
-        return Response({"Dowell Mail Service is running..."},status=status.HTTP_200_OK)
+        return Response({
+            "Dowell Mail Service is running..."
+        },status=status.HTTP_200_OK)
     
