e2e7e0cde91baf2dc5763c8f220726b66c537509
Manish <mdashsharma95@gmail.com>
2023-07-13 16:20:50 +0530

    Fixed the error message

--- originalityAI/views.py
+++ originalityAI/views.py
@@ -16,8 +16,7 @@ ORIGINAL_API_KEY = str(os.getenv('ORIGINAL_API_KEY'))
 
 @method_decorator(csrf_exempt, name='dispatch')
 class originalAITest(APIView):
-    def post(self, request,userapikey ):
-
+    def post(self, request, userapikey):
         api_key = ORIGINAL_API_KEY
         content = request.data.get('content')
         title = request.data.get('title')
@@ -31,77 +30,77 @@ class originalAITest(APIView):
         if serializer.is_valid():
             validate_api_count = processApikey(userapikey)
             data_count = json.loads(validate_api_count)
-            print("---data_count---",data_count)
-            if data_count['success'] :
-                if data_count['count'] >= 0 :
+            print("---data_count---", data_count)
+            if data_count['success']:
+                if data_count['count'] >= 0:
                     print("---Data count is ok---")
-                    response = originalAI(api_key,content, title)
+                    response = originalAI(api_key, content, title)
                     parsed_data = json.loads(response)
-                    print("---data_count---",parsed_data)
-                    try:
-                        if parsed_data['success']:
-                            originality_score = parsed_data['ai']['score']['original']
-                            ai_score = parsed_data['ai']['score']['ai']
-                            plagiarism_text_score = float(parsed_data['plagiarism']['total_text_score'].strip('%'))
+                    print("---parsed_data---", parsed_data)
 
-                            originality_score_percent = originality_score * 100
-                            ai_score_percent = ai_score * 100
-                            creative = 100 - plagiarism_text_score
+                    if 'success' in parsed_data and parsed_data['success']:
+                        originality_score = parsed_data['ai']['score']['original']
+                        ai_score = parsed_data['ai']['score']['ai']
+                        plagiarism_text_score = float(parsed_data['plagiarism']['total_text_score'].strip('%'))
 
-                            readability_stats = parsed_data['readability']['textStats']
-                            letter_count = readability_stats['letterCount']
-                            sentence_count = readability_stats['sentenceCount']
-                            paragraph_count = readability_stats['paragraphCount']
+                        originality_score_percent = originality_score * 100
+                        ai_score_percent = ai_score * 100
+                        creative = 100 - plagiarism_text_score
 
-                            if ai_score <= 0.10:
-                                category = "Written by Human"
-                            elif ai_score <= 0.30:
-                                category = "Most Probably by Human"
-                            elif ai_score <= 0.70:
-                                category = "Either by Human/AI"
-                            elif ai_score <= 0.90:
-                                category = "Most Probably by AI"
-                            else:
-                                category = "Written by AI"
+                        readability_stats = parsed_data['readability']['textStats']
+                        letter_count = readability_stats['letterCount']
+                        sentence_count = readability_stats['sentenceCount']
+                        paragraph_count = readability_stats['paragraphCount']
 
-                            return Response({
-                                "success": True,
-                                "message": "The test was successful",
-                                "Confidence level created by AI": f"{ai_score_percent:.2f}%",
-                                "Confidence level created by Human": f"{originality_score_percent:.2f}%",
-                                "AI Check":"{}".format(category),
-                                "Plagiarised":"{:.2f}%".format(plagiarism_text_score),
-                                "Creative": "{:.2f}%".format(creative),
-                                "Total characters": letter_count,
-                                "Total sentences": sentence_count,
-                                "Total paragraphs": paragraph_count,
-                                "credits": data_count['count'],
-                                "title": title,
-                                "content": content,
-                            },status=status.HTTP_200_OK)
+                        if ai_score <= 0.10:
+                            category = "Written by Human"
+                        elif ai_score <= 0.30:
+                            category = "Most Probably by Human"
+                        elif ai_score <= 0.70:
+                            category = "Either by Human/AI"
+                        elif ai_score <= 0.90:
+                            category = "Most Probably by AI"
                         else:
-                            return Response({
-                                "success": False,
-                                "message": "The test was not successful",
-                            },status=status.HTTP_200_OK)
-                    except:
+                            category = "Written by AI"
+
+                        return Response({
+                            "success": True,
+                            "message": "The test was successful",
+                            "Confidence level created by AI": f"{ai_score_percent:.2f}%",
+                            "Confidence level created by Human": f"{originality_score_percent:.2f}%",
+                            "AI Check": "{}".format(category),
+                            "Plagiarised": "{:.2f}%".format(plagiarism_text_score),
+                            "Creative": "{:.2f}%".format(creative),
+                            "Total characters": letter_count,
+                            "Total sentences": sentence_count,
+                            "Total paragraphs": paragraph_count,
+                            "credits": data_count['count'],
+                            "title": title,
+                            "content": content,
+                        }, status=status.HTTP_200_OK)
+                    elif 'error' in parsed_data:
                         return Response({
                             "success": False,
-                            "message": "Kindly contact to admin, This error will be solved soon",
-                        }, status=status.HTTP_400_BAD_REQUEST)
-                   
-                return Response({
-                    "success": False,
-                    "message": data_count['message'],
-                    "credits": data_count['count']
-                }, status=status.HTTP_200_OK)
+                            "message": parsed_data['error']
+                        })
+                    else:
+                        return Response({
+                            "success": False,
+                            "message": "The test was not successful",
+                        }, status=status.HTTP_200_OK)
+                else:
+                    return Response({
+                        "success": False,
+                        "message": data_count['message'],
+                        "credits": data_count['count']
+                    }, status=status.HTTP_200_OK)
             else:
                 return Response({
                     "success": False,
                     "message": data_count['message']
                 }, status=status.HTTP_400_BAD_REQUEST)
         else:
-                return Response({
-                    "success": False,
-                    "message": serializer._errors,
-                },status=status.HTTP_200_OK)
+            return Response({
+                "success": False,
+                "message": serializer.errors,
+            }, status=status.HTTP_200_OK)
