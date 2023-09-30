84ee73ba838856c73112bd1f1ac126f6a1cccba0
Manish <mdashsharma95@gmail.com>
2023-09-30 13:53:58 +0530

    url fixed

--- originalityAI/urls.py
+++ originalityAI/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('<str:userapikey>/', originalAITest.as_view()),
-    path('uxlivinglab/<str:userapikey>/', originalityConentTest.as_view()),
+    path('website-api/<str:userapikey>/', originalityConentTest.as_view()),
     path('', originalAITestInternal.as_view()),
 ]
