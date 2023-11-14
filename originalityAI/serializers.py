e625e708468cab6d27580841f2f0b06738b0ff6a
Manish <mdashsharma95@gmail.com>
2023-11-14 22:21:04 +0530

    removed email from the endpoints

--- originalityAI/serializers.py
+++ originalityAI/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 class APIInputCheckup(serializers.Serializer):
     content = serializers.CharField(allow_null=False, allow_blank=False)
     title = serializers.CharField(allow_null=False, allow_blank=False)
-    email = serializers.EmailField(allow_null=False, allow_blank=False)
 
 class WordCountValidator:
     def __init__(self, min_words):
