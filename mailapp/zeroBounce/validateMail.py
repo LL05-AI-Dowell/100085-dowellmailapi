7253c2fec56e1cedef392d0dfb9f4f38f2d0b7b8
Manish <mdashsharma95@gmail.com>
2024-05-13 16:50:11 +0530

    Saving email to database before validation

--- mailapp/zeroBounce/validateMail.py
+++ mailapp/zeroBounce/validateMail.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from database.datacube import *
+from datetime import datetime
 
 def validateMail(api_key, email):
     url = "https://api.zerobounce.net/v2/validate"
@@ -7,15 +9,68 @@ def validateMail(api_key, email):
 
     params = {"email": email, "api_key": api_key, "ip_address": ip_address}
 
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        response_data = json.loads(response.text)
-        if response_data["status"] == "valid" or response_data["status"] == "do_not_mail":
-            return {"status": "valid"}
-        else:
-            return {"status": "Invalid or risky email"}
-    else:
+    existingEmail = json.loads(datacube_data_retrieval(
+        "1b834e07-c68b-4bf6-96dd-ab7cdc62f07",
+        "emaildatabase",
+        "emaildata",
+        {
+            "email": email,
+        },
+        1,
+        0,
+        False
+
+    ))
+    print(existingEmail)
+    
+    if not existingEmail["success"]:
         return {"status": "Something went wrong"}
+    
+    if len(existingEmail["data"]) == 0:
+        print("The email is not present in the database")
+        checkEmail = requests.get(url, params=params)
+        if checkEmail.status_code == 200:
+            response_data = json.loads(checkEmail.text)
+            if response_data["status"] == "valid" or response_data["status"] == "do_not_mail":
+                insert_email_to_db = json.loads(datacube_data_insertion(
+                    "1b834e07-c68b-4bf6-96dd-ab7cdc62f07",
+                    "emaildatabase",
+                    "emaildata",
+                    {
+                        "email": email,
+                        "status": response_data["status"],
+                        "checked_on": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    }
+                ))
+
+                if not insert_email_to_db["success"]:
+                    return {"status": "Something went wrong"}
+                
+                return {"status": "valid"}
+            else:
+                insert_email_to_db = json.loads(datacube_data_insertion(
+                    "1b834e07-c68b-4bf6-96dd-ab7cdc62f07",
+                    "emaildatabase",
+                    "emaildata",
+                    {
+                        "email": email,
+                        "status": response_data["status"],
+                        "checked_on": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    }
+                ))
+
+                if not insert_email_to_db["success"]:
+                    return {"status": "Something went wrong"}
+                return {"status": "Invalid or risky email"}
+    
+    if existingEmail["data"][0]["status"] == "valid":
+        print("The email is already present in the database and it is valid email address")
+        return {"status": "valid"}
+    else:
+        print("The email is already present in the database and it is invalid email address")
+        return {"status":"Invalid or risky email"}
+    
+
 
 def emailFinder(api_key, domain, name):
     url = "https://api.zerobounce.net/v2/guessformat"
